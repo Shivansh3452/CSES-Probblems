674f1ce81ebc1bc3b0245ba20b228b96d6f51a44
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-12 16:44:13 +0530

    Refactor findLongestChain function and main logic

--- Maxm_len_pair_chain.cpp
+++ Maxm_len_pair_chain.cpp
@@ -20,6 +20,29 @@ int findLongestChain(vector<vector<int>>& pairs) {
     return count;
 }
 
+/*
+int t[1001][1001];
+    int lis(vector<vector<int>> &pairs,int prev,int curr){
+        if(curr==pairs.size())
+            return 0;
+        if(t[prev+1][curr]!=-1)
+            return t[prev+1][curr];
+        int taken=0;
+        if(prev==-1 ||pairs[curr][0]>pairs[prev][1]){
+            taken=1+lis(pairs,curr,curr+1);
+        }
+        int not_taken=lis(pairs,prev,curr+1);
+        return t[prev+1][curr]=max(taken,not_taken);
+    }
+
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n=pairs.size();
+        memset(t,-1,sizeof(t));
+        sort(begin(pairs),end(pairs));
+        return lis(pairs,-1,0);
+        }
+*/
+
 int main(){
     int n;
     cin>>n;     //pairs.size() is taken as input here
@@ -35,4 +58,5 @@ int main(){
     cout<<ans<<endl;
 
     return 0;
+
 }
